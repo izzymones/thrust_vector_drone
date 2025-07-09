5d17dbe1afd67065f4e2c8952b7852f8fe967178
wildthinkslaboratory <goldfishandrobin@gmail.com>
2025-07-09 16:14:29 -0700

    stubbing out a few more files

---
+++ animation.py
@@ -0,0 +1,7 @@
+# This is where we put our code for animating a simulation or real-time drone run
+
+# could be a class or just a function
+# should take an array of state data with input data and display animation of the 
+# behaviour.
+def animate(data):
+    pass
